b62c5786940ef937fbe9b8d73a7bef191570bea1
Garrett <Garrett@localhost>
2010-05-17 21:35:26 +0200

    Fixed ass.

--- FileContentFormatter/Properties/AssemblyInfo.cs
+++ FileContentFormatter/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FileContentFormatter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Keeper.Garrett.ScrewTurn.FileContentFormatter")]
+[assembly: AssemblyDescription("Plugin for the ScrewTurn Wiki http://www.screwturn.eu/. Plugin can be obtained from http://keeper.endoftheinternet.org.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("FileContentFormatter")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("The Keepers")]
+[assembly: AssemblyProduct("ScrewTurn.FileContentFormatter")]
+[assembly: AssemblyCopyright("Copyright © Christian Hollerup Mikkelsen 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.*")]
