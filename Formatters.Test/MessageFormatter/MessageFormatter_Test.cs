55da16e2e765d166658fafc101ae58037a5efc31
Garrett <Garrett@localhost>
2010-05-24 23:13:55 +0200

    Added unittests

--- Formatters.Test/MessageFormatter/MessageFormatter_Test.cs
+++ Formatters.Test/MessageFormatter/MessageFormatter_Test.cs
@@ -174,5 +174,39 @@ namespace Formatters.Tests
             //Assert
             Assert.AreEqual("bla bla bla <link type=\"text/css\" rel=\"stylesheet\" href=\"GetFile.aspx?File=/Keeper.Garrett.Formatters/MessageFormatter/MessageStyle.css\"></link>\n<table class=\"tip\"><tbody>\n<tr><td class=\"image-col\"></td><td class=\"head-col\">Tip</td</tr></tbody></table> bla bla bla", retval);
         }
+
+        [Test]
+        public void StoreFiles_CreateAll()
+        {
+            //Arrange
+            var formatter = new MessageFormatter();
+
+            //Arrange
+            var host = MockRepository.GenerateMock<IHostV30>();
+            var provider = MockRepository.GenerateStub<IFilesStorageProviderV30>();
+
+            provider.Expect(x => x.Information).Return(new ComponentInformation("SomeProvider", "", "", "", ""));
+            host.Expect(x => x.GetSettingValue(SettingName.DefaultFilesStorageProvider)).IgnoreArguments().Return(provider.GetType().FullName);
+            host.Expect(x => x.GetFilesStorageProviders(true)).IgnoreArguments().Return(new IFilesStorageProviderV30[] { provider });
+
+            //Dirs
+            provider.Expect(x => x.ListDirectories("/")).Return(new string[] { "NoPresent1" });
+            provider.Expect(x => x.ListDirectories("/Keeper.Garrett.Formatters/")).Return(new string[] { "NoPresent2" });
+            provider.Expect(x => x.ListDirectories(string.Format("/Keeper.Garrett.Formatters/{0}", formatter.GetType().Name))).Return(new string[] { "NoPresent3" });
+
+            //Files
+            provider.Expect(x => x.ListFiles(string.Format("/Keeper.Garrett.Formatters/{0}", formatter.GetType().Name))).Return(new string[] { "NoFilesAtAll" });
+            provider.Expect(x => x.ListFiles(string.Format("/Keeper.Garrett.Formatters/{0}/Images-24x24", formatter.GetType().Name))).Return(new string[] { "NoFilesAtAll" });
+            provider.Expect(x => x.ListFiles(string.Format("/Keeper.Garrett.Formatters/{0}/Images-32x32", formatter.GetType().Name))).Return(new string[] { "NoFilesAtAll" });
+
+            //Act
+            formatter.Init(host, "");
+
+            //Assert
+            provider.AssertWasCalled(x => x.CreateDirectory("/", "Keeper.Garrett.Formatters"));
+            provider.AssertWasCalled(x => x.CreateDirectory("/Keeper.Garrett.Formatters/", "MessageFormatter"));
+            provider.AssertWasCalled(x => x.CreateDirectory("/Keeper.Garrett.Formatters/MessageFormatter", "Images-24x24"));
+            provider.AssertWasCalled(x => x.CreateDirectory("/Keeper.Garrett.Formatters/MessageFormatter", "Images-32x32"));
+        }
     }
 }
